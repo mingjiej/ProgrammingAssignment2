be326362424b8a509bd77da3f4216be9f845438f
mingjiej <jackpot0704@gmail.com>
2016-05-15 16:23:35 -0700

    add hw2
    add hw2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+          x <<- y
+          i <<- NULL
+    }
+    get <- function() x
+    setinvert <- function(invert) i <<- invert
+    getinvert <- function() i
+    list(set = set, get = get,
+    setinvert = setinvert,
+    getinvert = getinvert)
 }
 
 
@@ -12,4 +22,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getinvert()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    if(nrow(data)!=ncol(data)) {
+        message("cannot invert, matrix is not a square matrix")
+        return(i)
+    }
+    i <- solve(data, ...)
+    x$setinvert(i)
+    i
 }
